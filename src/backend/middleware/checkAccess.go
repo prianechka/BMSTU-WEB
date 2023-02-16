ebe7f961e78bff8c5c02f824f9b966d548d0147a
Alexander Prianishnikov <mrpriany@mail.ru>
2023-02-17 00:11:59 +0300

    Complete Lab1 with other tasks

--- src/backend/middleware/checkAccess.go
+++ src/backend/middleware/checkAccess.go
@@ -9,12 +9,21 @@ import (
 
 func CheckAccess(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		accessToken := r.Header.Get("access-token")
-		role := jwtUtils.GetRoleFromJWT(accessToken)
-		if access.CheckRoleAccess(r.RequestURI, r.Method, role) {
+		if r.RequestURI == "/test" {
+			next.ServeHTTP(w, r)
+		} else if r.RequestURI == "/status" {
+			next.ServeHTTP(w, r)
+		} else if r.RequestURI == "/next/index1.html/" || r.RequestURI == "/next/style1.css/" ||
+			r.RequestURI == "/next/image1.jpg/" || r.RequestURI == "/index1.html/" {
 			next.ServeHTTP(w, r)
 		} else {
-			http.Error(w, objects.ForbiddenErrorString, http.StatusForbidden)
+			accessToken := r.Header.Get("access-token")
+			role := jwtUtils.GetRoleFromJWT(accessToken)
+			if access.CheckRoleAccess(r.RequestURI, r.Method, role) {
+				next.ServeHTTP(w, r)
+			} else {
+				http.Error(w, objects.ForbiddenErrorString, http.StatusForbidden)
+			}
 		}
 	})
 }
